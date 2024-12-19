32513530edb00cd2c82939ac2678fa2eef9d1bbf
Marcelo Marques de Lima <111276703+MarceloMarquesdeLima@users.noreply.github.com>
2024-12-18 22:23:06 -0300

    Inicio do projeto
    Iniciando a estrutura inicial do projeto backend

---
+++ src/main/java/com/mqs/Api_cooperativismo_voto/ApiCooperativismoVotoApplication.java
@@ -0,0 +1,13 @@
+package com.mqs.Api_cooperativismo_voto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCooperativismoVotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCooperativismoVotoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mqs/Api_cooperativismo_voto/ApiCooperativismoVotoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mqs.Api_cooperativismo_voto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCooperativismoVotoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
